3548ccdcbfee61adf745deadd75edda43e07e4bf
BrianLevin <bml201095@gmail.com>
2021-05-06 12:36:10 -0400

    added process.env.port 300 for sever to connec tto heroku

--- app.js
+++ app.js
@@ -224,6 +224,6 @@ Post.findOne({_id: requestedPostId}, function(err, post){
 
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT|| 3000, function() {
   console.log("Server started on port 3000");
 });
