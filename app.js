4ab914a23f56708f46ed5030625043e7a4627478
BrianLevin <bml201095@gmail.com>
2021-05-06 12:21:52 -0400

    added link to connect to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,84 @@
-//jshint esversion:6
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const ejs = require("ejs");
+// const mongoose = require('mongoose');
+
+// const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+// const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+// const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+// const app = express();
+
+// app.set('view engine', 'ejs');
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static("public"));
+
+// mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+// const postSchema = {
+//   title: String,
+//   content: String
+// };
+
+// const Post = mongoose.model("Post", postSchema);
+
+// app.get("/", function(req, res){
+
+//   Post.find({}, function(err, posts){
+//     res.render("home", {
+//       startingContent: homeStartingContent,
+//       posts: posts
+//       });
+//   });
+// });
+
+// app.get("/compose", function(req, res){
+//   res.render("compose");
+// });
+
+// app.post("/compose", function(req, res){
+//   const post = new Post({
+//     title: req.body.postTitle,
+//     content: req.body.postBody
+//   });
+
+
+//   post.save(function(err){
+//     if (!err){
+//         res.redirect("/");
+//     }
+//   });
+// });
+
+// app.get("/posts/:postId", function(req, res){
+
+// const requestedPostId = req.params.postId;
+
+//   Post.findOne({_id: requestedPostId}, function(err, post){
+//     res.render("post", {
+//       title: post.title,
+//       content: post.content
+//     });
+//   });
+
+// });
+
+// app.get("/about", function(req, res){
+//   res.render("about", {aboutContent: aboutContent});
+// });
+
+// app.get("/contact", function(req, res){
+//   res.render("contact", {contactContent: contactContent});
+// });
+
+
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
+
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -21,8 +101,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-// connect mongoose
-mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+// connect mongoose //connect to atlas
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
 
 // schema blueprint
 const postSchema = {
@@ -96,23 +176,25 @@ app.post("/compose", function (req,res){
       if (!err) {
 res.redirect("/");
 
+      } else{
+// redirect to homepage after posts are p
+res.redirect("/");
       }
     });
-  // redirect to homepage after posts are p
-    res.redirect("/");
+  
 })
 
 
 // router which will go to individual post pages
-app.get ("/posts/:postID", function(req,res) {
+app.get ("/posts/:postId", function(req,res) {
 // req.params= access to all values with a colen in front of it
 // lodash to convert everything typed in search bar to view post  to lowercase
 // store postID parameter
 const requestedPostId = req.params.postId;
-
+// const requestedTitle= _.lowerCase(req.params.postName);
 // find one method to find posts witch matching with matching id in posts collection
 Post.findOne({_id: requestedPostId}, function(err, post){
-// render content to ejs page
+// // render content to ejs page
   res.render("post", {
 
     title: post.title,
@@ -122,7 +204,23 @@ Post.findOne({_id: requestedPostId}, function(err, post){
   });
 
 });
+// loop through all posts in post array
+// posts.forEach(function(post){
 
+// //   // saved  for each stored post
+//   const storedTitle= _.lowerCase(post.title);
+
+// //   // check if they match
+
+//   if(storedTitle ===  requestedTitle) {
+// // render post page if the stored title === requested title
+//     res.render("post",{
+//       title: post.title,
+//       content: post.content
+//     });
+//   } 
+
+// });
 
 });
 
